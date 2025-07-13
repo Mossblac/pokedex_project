8245c39d3e387382b70ef827721e77db70a7b9bc
Mossblac <Murphyrmurphy2000@gmail.com>
2025-07-12 21:22:46 -0600

    REPL created and working

--- main.go
+++ main.go
@@ -1,12 +1,26 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 	"strings"
 )
 
 func main() {
-	fmt.Println("Hello World!")
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Printf("Pokedex > ")
+		scanner.Scan()
+		input := scanner.Text()
+		if input != "" {
+			input = strings.ToLower(input)
+			fields := strings.Fields(input)
+			firstWord := fields[0]
+			fmt.Printf("Your command was: %v\n", firstWord)
+		}
+
+	}
 }
 
 func cleanInput(text string) []string {
