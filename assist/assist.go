2f9a9f7bae926d5884d9eac69ec537715ea29f66
Mossblac <Murphyrmurphy2000@gmail.com>
2025-07-15 07:40:11 -0600

    working on CommandMap callback

--- assist/assist.go
+++ assist/assist.go
@@ -16,9 +16,24 @@ func CleanInput(text string) []string {
 
 }
 
+var CommandInfo map[string]CliCommand
+
 func CommandExit() error {
 	fmt.Println("Closing the Pokedex... Goodbye!")
 	os.Exit(0)
+	return nil
+}
+
+func CommandHelp() error {
+	fmt.Printf("Welcome to the Pokedex!\nUsage:\n")
+	for _, cmd := range CommandInfo {
+		fmt.Printf("%s: %s\n", cmd.Name, cmd.Description)
+	}
+	return nil
+}
+
+func CommandMap() error {
+	// each call prints 20 locations at a time
 	return nil
 }
 
@@ -26,12 +41,30 @@ type CliCommand struct {
 	Name        string
 	Description string
 	Callback    func() error
+	Page        *Config
 }
 
-var CommandMap = map[string]CliCommand{
-	"exit": {
-		Name:        "exit",
-		Description: "EXit the Pokedex",
-		Callback:    CommandExit,
-	},
+type Config struct {
+	Next     string
+	Previous string
+}
+
+func init() {
+	CommandInfo = map[string]CliCommand{
+		"exit": {
+			Name:        "exit",
+			Description: "EXit the Pokedex",
+			Callback:    CommandExit,
+		},
+		"help": {
+			Name:        "help",
+			Description: "Displays a help message",
+			Callback:    CommandHelp,
+		},
+		"map": {
+			Name:        "map",
+			Description: "Displays 20 locations per call",
+			Callback:    CommandMap,
+		},
+	}
 }
