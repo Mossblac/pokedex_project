9524cc571db7b30a013cd14467c8340bd0aac816
Mossblac <Murphyrmurphy2000@gmail.com>
2025-07-17 08:58:08 -0600

    outlined pseudocode- in internal.go

---
+++ pokedex/internal/internalpackage/internalpackage.go
@@ -0,0 +1,10 @@
+package internalpackage
+
+//use get to obtain list of bytes.
+//save theose bytes to a file within internal folder
+//use os package to read file and apply to a variable
+//use unmarshal to convert the slice of bytes into a Go struct
+//import the Go struct into assist.go
+//split list into 20 entry chunks
+//apply each chunk the command struct under Next and Previous
+//write CommandMap and CommandMap back to access the pages
